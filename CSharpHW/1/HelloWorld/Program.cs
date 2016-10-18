dcea463e7361becf2149df732b9eb682a73f2287
manyak-yuriy <manyak.yuriy@gmail.com>
2016-10-18 18:00:52 +0300

    Initial commit

---
+++ CSharpHW/1/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hello World and Yuriy!");
+            Console.ReadKey();
+        }
+    }
+}
