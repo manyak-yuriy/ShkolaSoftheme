94e70de85bf7adf1c59566dba42a47eafa080df0
manyak-yuriy <manyak.yuriy@gmail.com>
2016-11-21 07:09:56 +0200

    Task 26 finished

---
+++ CSharpHW/26/MultiThreading/MultiThreading/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+using System.IO.Compression;
+using MultiThreading;
+
+namespace TPLMultiTasking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDir = @"D:\test";
+
+            Console.WriteLine("Input path: ");
+            string filePath = Console.ReadLine();
+
+            Console.WriteLine("Input source string: ");
+            string source = Console.ReadLine();
+
+            Console.WriteLine("Input destination string: ");
+            string dest = Console.ReadLine();
+
+            if (filePath.Length == 0)
+                filePath = rootDir;
+
+            StringReplacer.ReplaceInDir(filePath, source, dest, new string[] {".dat", ".txt"});
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ CSharpHW/26/MultiThreading/MultiThreading/StringReplacer.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreading
+{
+    public static class StringReplacer
+    {
+        public static object syncObj = 0;
+        public static void ReplaceInDir(string rootPath, string source, string dest, string[] suffixFilter)
+        {
+
+            if (!Directory.Exists(rootPath))
+            {
+                throw new ArgumentException("Invalid path name: {0}", rootPath);
+            }
+
+            Queue<string> directories = new Queue<string>();
+
+            directories.Enqueue(rootPath);
+
+            while (directories.Any())
+            {
+                string currentDir = directories.Dequeue();
+
+                string[] nestedDirs = { };
+                string[] files = { };
+
+                try
+                {
+                    nestedDirs = Directory.GetDirectories(currentDir);
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine(exc.Message);
+                }
+
+                try
+                {
+                    files = Directory.GetFiles(currentDir);
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine(exc.Message);
+                }
+
+
+                foreach (var file in files)
+                {
+                    Task.Factory.StartNew(() => { ReplaceInFile(file, source, dest, suffixFilter); });
+                }
+
+
+                foreach (var dir in nestedDirs)
+                {
+                    directories.Enqueue(dir);
+                }
+            }
+
+        }
+
+        public static void ReplaceInFile(string fileName, string source, string dest, string[] suffixFilter)
+        {
+            bool passed = false;
+
+            foreach (var filter in suffixFilter)
+                if (fileName.EndsWith(filter))
+                {
+                    passed = true;
+                    break;
+                }
+
+            if (!passed)
+                return;
+
+            StringBuilder logBuf = new StringBuilder();
+            Console.WriteLine("Reading from {0}", fileName);
+            string[] lines = File.ReadAllLines(fileName);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string newLine = lines[i].Replace(source, dest);
+                if (lines[i] != newLine)
+                {
+                    logBuf.Append(String.Format("File: {0}", fileName));
+                    logBuf.Append(String.Format("   -- {0}", lines[i]));
+                    logBuf.Append(String.Format("   ++ {0}", newLine));
+                    logBuf.Append(Environment.NewLine);
+
+                    lines[i] = newLine;
+                }
+
+            }
+            
+            File.WriteAllLines(fileName, lines);
+            Console.WriteLine("Writing to {0}", fileName);
+
+            lock (syncObj)
+            {
+                using (StreamWriter logFile = File.AppendText(@"D:\test\log.txt"))
+                {
+                    logFile.Write(logBuf.ToString());
+                }
+            }
+
+        }
+    }
+}
